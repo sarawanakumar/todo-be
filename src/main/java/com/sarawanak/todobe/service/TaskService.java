de555f030a5c9f2e1b3f3f60050ed4882fe5822a
Saravanakumar Selladurai <ssarawanakumar@gmail.com>
2020-04-15 20:00:52 +0530

    Add jackson dependency

--- src/main/java/com/sarawanak/todobe/service/TaskService.java
+++ src/main/java/com/sarawanak/todobe/service/TaskService.java
@@ -1,4 +1,11 @@
 package com.sarawanak.todobe.service;
 
+import com.sarawanak.todobe.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
 }
--- src/main/java/com/sarawanak/todobe/service/UserService.java
+++ src/main/java/com/sarawanak/todobe/service/UserService.java
@@ -1,4 +1,11 @@
 package com.sarawanak.todobe.service;
 
+import com.sarawanak.todobe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 }
