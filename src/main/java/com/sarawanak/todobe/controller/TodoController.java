162c52a6663d43edb264206258915677b7510b3b
Saravanakumar Selladurai <ssarawanakumar@gmail.com>
2020-04-15 13:14:18 +0530

    Initial Commit. Basic Project setup with /hello

---
+++ src/main/java/com/sarawanak/todobe/controller/TodoController.java
@@ -0,0 +1,13 @@
+package com.sarawanak.todobe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/sarawanak/todobe/TodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarawanak.todobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
