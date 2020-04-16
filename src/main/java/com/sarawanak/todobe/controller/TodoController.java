83e873661e8c57d3ca76ead2fca96300df54d1ca
Saravanakumar Selladurai <ssarawanakumar@gmail.com>
2020-04-16 13:22:11 +0530

    Add /todo/{id}

--- src/main/java/com/sarawanak/todobe/controller/TodoController.java
+++ src/main/java/com/sarawanak/todobe/controller/TodoController.java
@@ -4,10 +4,12 @@ import com.sarawanak.todobe.model.Task;
 import com.sarawanak.todobe.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -25,12 +27,17 @@ public class TodoController {
         return taskService.getAllTodos();
     }
 
+    @GetMapping("/todo/{id}")
+    public Optional<Task> getTodo(@PathVariable String id) {
+        return taskService.getTodoById(id);
+    }
+
     @GetMapping(value = "/todo/filter")
     public List<Task> getTodosFor(
         @RequestParam(name = "user_id", required = false) Integer userId,
         @RequestParam(required = false) String priority,
         @RequestParam(required = false) String status
     ) {
-        return taskService.getTodosByUserId(userId, priority, status);
+        return taskService.getTodosMatching(userId, priority, status);
     }
 }
--- src/main/java/com/sarawanak/todobe/service/TaskService.java
+++ src/main/java/com/sarawanak/todobe/service/TaskService.java
@@ -3,13 +3,12 @@ package com.sarawanak.todobe.service;
 import com.sarawanak.todobe.helper.PriorityHelper;
 import com.sarawanak.todobe.helper.StatusHelper;
 import com.sarawanak.todobe.model.Task;
-import com.sarawanak.todobe.repository.CustomTaskRepositoryImpl;
 import com.sarawanak.todobe.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -20,7 +19,12 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    public List<Task> getTodosByUserId(Integer userId, String priority, String status) {
+    public Optional<Task> getTodoById(String id) {
+        Integer taskId = Integer.parseInt(id);
+        return taskRepository.findById(taskId);
+    }
+
+    public List<Task> getTodosMatching(Integer userId, String priority, String status) {
         Integer priorityCode = PriorityHelper.getCodeForPriority(priority);
         Integer statusCode = StatusHelper.getCodeForStatus(status);
 
